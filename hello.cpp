eb4a96e1126383a49b3b608df1fd51bec959dcca
Justin Chervony <justinchervony@gmail.com>
2026-04-06 18:26:04 -0400

    Started cpp logic practice and refresh with audio examples

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+
+float midiToFrequency (int midiNote){
+    return 440.0f * std::pow(2.0f, (midiNote - 69) / 12.0f);
+}
 
 int main () {
-    std::cout << "Hello, audio world! Testing!" << std::endl;
+    int sampleRate = 44100;
+    float frequency = 440.0;
+    bool isPlaying = true;
+    float freq = midiToFrequency(69);
+
+    std::cout << sampleRate << std::endl;
+    std::cout << frequency << std::endl;
+    std::cout << isPlaying << std::endl;
+    std::cout << freq << std::endl;
+
     return 0;
 }
